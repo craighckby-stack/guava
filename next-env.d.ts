1590213ca84a397efd7405b76a9f694d7e704a9e
Craig Huckerby <craighckby@gmail.com>
2026-08-02 23:10:57 +1000

    [DARLEK CANN] Mutate next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,10 @@
+/**
+ * DARLEK CANN ARCHITECTURAL HEADER
+ * File: next-env.d.ts
+ * Role: Core system component participating in autonomous cognitive evolution cycles.
+ * Architecture: Type-safe modular unit with resilient state interfaces.
+ */
+
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import "./.next/types/routes.d.ts";
