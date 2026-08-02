ce68cd2e49cf65a07acba0dc6bd919cb602d1e04
Craig Huckerby <craighckby@gmail.com>
2026-08-02 23:05:00 +1000

    [DARLEK CANN] Mutate examples/websocket/server.ts

--- examples/websocket/server.ts
+++ examples/websocket/server.ts
@@ -1,3 +1,10 @@
+/**
+ * DARLEK CANN ARCHITECTURAL HEADER
+ * File: examples/websocket/server.ts
+ * Role: Core system component participating in autonomous cognitive evolution cycles.
+ * Architecture: Type-safe modular unit with resilient state interfaces.
+ */
+
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 
